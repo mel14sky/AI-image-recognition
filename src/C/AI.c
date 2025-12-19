0ca3875bc08d25cada551569ed06d4444ae673e1
18smi <18smith.ben@gmail.com>
2025-12-19 16:03:06 +0000

    small

--- src/C/AI.c
+++ src/C/AI.c
@@ -11,7 +11,7 @@
 
 int main(void) {
 
-    //struct AI* ai = create_AI_from_txt("output.txt");
+    //struct AI* ai = create_AI_from_txt("build/output.txt");
     int layers_arr[number_of_layers] = {2, 5, 1};
     struct AI* ai = create_AI(layers_arr, number_of_layers, 0.05);
     randomize_AI_weights(ai);
@@ -24,7 +24,7 @@ int main(void) {
 
 
 
-    save_AI_to_txt(ai, "output.txt");
+    save_AI_to_txt(ai, "build/output.txt");
     free_AI(ai);
     printf("freed\n");
 }

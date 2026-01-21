1aa9ee8fee02e1a65b98f6ffe75327bdd3f5a8af
amahamoud177 <a.mahamoud177@gmail.com>
2026-01-21 16:55:59 +0000

    Add files via upload

--- src/Python/training.py
+++ src/Python/training.py
@@ -8,6 +8,7 @@ path_name = "archivegen1/dataset"
 # Function to convert images to BMP format
 def convert_bmp(filename):
     try:
+        filename = os.path.abspath(filename)
         with Image.open(filename) as img:
             file_root, file_ext = os.path.splitext(filename)
             bmp_filename = f"{file_root}.bmp"
@@ -26,6 +27,32 @@ def get_folder_path(target_folder_name):
     if os.path.isdir(full_path):
         return full_path
 
+def vector2txt(vector, name):
+    if vector is None:
+        print("No vector to save.")
+        return
+    name = os.path.abspath(name)
+    pokemon_folder = os.path.dirname(name)
+    txt_folder = os.path.join(pokemon_folder, "vectors")
+    os.makedirs(txt_folder, exist_ok=True)
+    image_name = os.path.splitext(os.path.basename(name))[0]
+    output_path = os.path.join(txt_folder, f"{image_name}.txt")
+    try:
+        with open(output_path, 'w') as f:
+            #write length of vector + 1 for the bias
+            f.write(f"{len(vector)+1}\n")
+
+            for value in vector:
+                # force vector values to be 6dp
+                formatted_value = f"{value:.6f}"
+                f.write(f"{formatted_value}\n")
+            # force last element to be 1.000000
+            f.write("1.000000")
+
+    except IOError as error:
+        print(f"Error writing to output file: {error}")
+        return
+
 def main():
 # reads order.txt and checks for images in each folder and prints out the file names
     if os.path.exists("order.txt"):
@@ -37,19 +64,21 @@ def main():
             if not pokemon_name:
                 continue
             print(f"Checking folder: {pokemon_name}")
-    # finds the folder path and lists files inside
+            # finds the folder path and lists files inside
             folder_path = get_folder_path(pokemon_name)
             if folder_path:
-                files_inside = os.listdir(folder_path)
+                files_inside = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f)) and f.lower() != "vectors"]
                 if len(files_inside) == 0:
                     print(f"The folder '{pokemon_name}' is empty.")
                 else:
-            # Converts images to BMP format
-                    print(f"Found {len(files_inside)} files inside:")
+                # Converts images to BMP format
                     for filename in files_inside:
-                        path = path_name + "/" + pokemon_name + "/" + filename
-                        convert_bmp(path)
-                        img2vec.main(path)
+                        # path = path_name + "/" + pokemon_name + "/" + filename
+                        current_path = os.path.join(path_name, pokemon_name, filename)
+                        bmp_path = convert_bmp(current_path)
+                        if bmp_path:
+                            vector = img2vec.image2vector(bmp_path)
+                            vector2txt(vector, bmp_path)
                     print("Complete")
             else:
                 print(f"FAILURE: Folder '{pokemon_name}' not found.")
